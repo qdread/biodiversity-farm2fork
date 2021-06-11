f1c2861bb555699a6ed62397fe75f1da57c9d758
qdread <qread@sesync.org>
2021-06-11 11:06:33 -0400

    Add world map

--- app.R
+++ app.R
@@ -10,6 +10,9 @@ library(gridExtra)
 
 # FIXME In general, can this all be sped up by running portions of code only when needed? i.e. make more reactive? Not sure.
 
+# FIXME Another general fix: instead of returning errors for invalid combinations of input, either return an informative message
+# FIXME or better yet, "gray out" invalid possibilities so that users can't select something invalid.
+
 # FIXME Could consider different layouts, for example a "dashboard" style layout with multiple panels for plot, table, and map, instead of tabs.
 
 # FIXME There are some bugs in this, possibly related to data.table modifying by reference... sometimes you get a different result if
@@ -155,7 +158,7 @@ ui <- fluidPage(
 # FIXME Why the hell should that make any difference? The function is still CALLED in the same place, it's just defined in a different file.
 
 # This function is called within each of the three tabs to return data to be plotted
-prepare_data <- function(input) {
+prepare_data <- function(input, tab_id) {
     scale_fn <- ifelse(input[['log_scale']], scale_y_log10, scale_y_continuous)
     flow_type <- input[['flow_type']]
     
@@ -167,48 +170,77 @@ prepare_data <- function(input) {
     # Subset rows based on selected subcategories.
     # Calculate sum of flows by scenarios, summing across all selected subcategories.
     if (flow_type == 'goods') {
-        # FIXME Inbound foreign goods will be in units of tonnes.
+        # FIXME Inbound foreign goods will be in units of tonnes. Currently not supported.
         
-        dat <- copy(county_goods_flow_sums)
         fill_var <- 'ag_good_short_name'
         scale_name <- 'Agricultural goods category'
         y_name <- 'Agricultural goods flow (million USD)'
-        dat_plot <- dat[BEA_code %in% input[['goods_subcats']], 
-                        lapply(.SD, sum), by = .(ag_good_short_name, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
-        dat_map <- dat[BEA_code %in% input[['goods_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']], 
-                       lapply(.SD, sum), by = .(county, ag_good_short_name, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+        if (tab_id %in% c('plot', 'table')) {
+            dat <- copy(county_goods_flow_sums)
+            dat <- dat[BEA_code %in% input[['goods_subcats']], 
+                       lapply(.SD, sum), by = .(ag_good_short_name, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+        } else {
+            if (input[['map_type']] == 'usa') {
+                dat <- copy(county_goods_flow_sums)
+                dat <- dat[BEA_code %in% input[['goods_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']], 
+                           lapply(.SD, sum), by = .(county, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+            } else {
+                # FIXME Currently not supported to show foreign goods flows on map.
+            }
+        }
     }
     
     if (flow_type == 'land') {
-        dat <- copy(county_land_flow_sums)
+        
         fill_var <- 'land_type'
         scale_name <- 'Land use category'
         y_name <- parse(text = 'Land~flow~(km^2)')
-        dat_plot <- dat[land_type %in% input[['land_subcats']], 
-                        lapply(.SD, sum), by = .(land_type, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
-        dat_map <- dat[land_type %in% input[['land_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']], 
-                       lapply(.SD, sum), by = .(county, land_type, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+        if (tab_id %in% c('plot', 'table')) {
+            dat <- copy(county_land_flow_sums)
+            dat <- dat[land_type %in% input[['land_subcats']], 
+                       lapply(.SD, sum), by = .(land_type, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+        } else {
+            if (input[['map_type']] == 'usa') {
+                dat <- copy(county_land_flow_sums)
+                dat <- dat[land_type %in% input[['land_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']], 
+                           lapply(.SD, sum), by = .(county, land_type, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+            } else {
+                dat <- copy(foreign_land_flow_sums)
+                dat <- dat[land_type %in% input[['land_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']],
+                           lapply(.SD, sum), by = .(ISO_A3, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+            }
+        }
     }
     
     if (flow_type == 'biodiv') {
-        dat <- copy(county_extinction_flow_sums)
+        
         fill_var <- 'taxon'
         scale_name <- 'Taxonomic group'
         y_name <- 'Biodiversity threat flow (potential extinctions)'
-        dat_plot <- dat[land_type %in% input[['land_subcats']] & taxon %in% input[['taxa_subcats']], 
-                        lapply(.SD, sum), by = .(taxon, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
-        dat_map <- dat[land_type %in% input[['land_subcats']] & taxon %in% input[['taxa_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']], 
-                       lapply(.SD, sum), by = .(county, taxon, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+        if (tab_id %in% c('plot', 'table')) {
+            dat <- copy(county_extinction_flow_sums)
+            dat <- dat[land_type %in% input[['land_subcats']] & taxon %in% input[['taxa_subcats']], 
+                       lapply(.SD, sum), by = .(taxon, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+        } else {
+            if (input[['map_type']] == 'usa') {
+                dat <- copy(county_extinction_flow_sums)
+                dat <- dat[land_type %in% input[['land_subcats']] & taxon %in% input[['taxa_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']], 
+                           lapply(.SD, sum), by = .(county, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline)]
+            } else {
+                dat <- copy(foreign_extinction_flow_sums)
+                dat <- dat[land_type %in% input[['land_subcats']] & taxon %in% input[['taxa_subcats']] & scenario_diet %in% input[['scenario_diet']] & scenario_waste %in% input[['scenario_waste']],
+                           lapply(.SD, sum, by = .(ISO_A3, scenario_diet, scenario_waste), .SDcols = c(col_value, col_baseline))]
+            }
+        }
     }
     
     # Normalize value by baseline if selected
     if (input[['normalize']]) {
-        set(dat_plot, j = col_value, value = dat_plot[[col_value]]/dat_plot[[col_baseline]])
-        set(dat_map, j = col_value, value = dat_map[[col_value]]/dat_map[[col_baseline]])
+        set(dat, j = col_value, value = dat[[col_value]]/dat[[col_baseline]])
         y_name <- 'Flow relative to baseline scenario'
     }
     
-    return(list(data = dat_plot, data_map = dat_map, scale_fn = scale_fn, fill_var = fill_var, scale_name = scale_name, y_name = y_name, col_value = col_value, col_baseline = col_baseline))
+    return(list(data = dat, scale_fn = scale_fn, fill_var = fill_var, scale_name = scale_name, y_name = y_name, col_value = col_value, col_baseline = col_baseline))
 }
 
 
@@ -217,7 +249,7 @@ prepare_data <- function(input) {
 server <- function(input, output) {
     
     output$plot <- renderPlot({
-        tab_data <- prepare_data(input)
+        tab_data <- prepare_data(input, tab_id = 'plot')
         
         # FIXME Possibly include two plots: a dodged or stacked geom_col so you can see each one separately, and 
         # FIXME a summed geom_col for the total of all categories selected.
@@ -236,7 +268,7 @@ server <- function(input, output) {
     })
     
     output$table <- render_gt({
-       tab_data <- prepare_data(input)
+       tab_data <- prepare_data(input, tab_id = 'table')
        tab_data[['data']][, grep('baseline', names(tab_data[['data']]), value = TRUE) := NULL]
        
        # Use long names for diet and waste scenarios in table.
@@ -261,8 +293,6 @@ server <- function(input, output) {
            data_color(tab_data[['col_value']], colors = color_palette, alpha = 0.75) %>%
            fmt_number(columns = 4, n_sigfig = 3)
        
-       # FIXME Change the color palette to something divergent, centered at no change (0 or 1), if normalized input is selected.
-       
        # FIXME Improve column labels (include units on Flow)
        # FIXME Any improvements to formatting possible?
        
@@ -271,19 +301,17 @@ server <- function(input, output) {
     })
     
     output$map <- renderPlot({
-        tab_data <- prepare_data(input)
         
-        tab_data[['data_map']] <- merge(county_map, tab_data[['data_map']], by = 'county')
-        # FIXME Currently this is only the USA map but later we would need to add in the foreign map for those cases where it's needed
-
-        # Get scale to be used across all three maps
-        vals <- tab_data[['data_map']][[tab_data[['col_value']]]]
+        tab_data <- prepare_data(input, tab_id = 'map')
+        
+        # Get scale to be used across all maps
+        vals <- tab_data[['data']][[tab_data[['col_value']]]]
         if (input[['log_scale']]) scale_range <- range(vals[vals > 0], na.rm = TRUE) else scale_range <- range(vals, na.rm = TRUE)
         
         # Use color scale depending on whether divergent or sequential is needed
         if (input[['normalize']]) {
             # FIXME Uncomment the below line if we want to center at zero, then change to center=0
-            # set(tab_data[['data_map']], j = tab_data[['col_value']], value = tab_data[['data_map']][[tab_data[['col_value']]]] - 1)
+            # set(tab_data[['data']], j = tab_data[['col_value']], value = tab_data[['data']][[tab_data[['col_value']]]] - 1)
             # Remap scale range so that it is centered at 1.
             fill_scale_range_remap <- scale_begin_end(vals, center = 1)
             
@@ -291,30 +319,49 @@ server <- function(input, output) {
         } else {
             color_scale <- scale_fill_viridis_c(name = tab_data[['y_name']], trans = ifelse(input[['log_scale']], 'log10', 'identity'), limits = scale_range)
         }
+        
+        if (input[['map_type']] == 'world') {
+            if (input[['flow_origin']] == 'inbound') {
+                # FIXME Here return an informative message that only production can be shown for foreign countries.
+            } else {
 
-        p48 <- ggplot() +
-            geom_sf(data = subset(tab_data[['data_map']], !fips_state %in% c('02', '15')), aes_string(fill = tab_data[['col_value']]), size = 0.25) +
-            color_scale +
-            map_theme +
-            theme(legend.position = 'top', legend.key.width = unit(1.2, 'cm'))
-        
-        pak <- ggplot() +
-            geom_sf(data = subset(tab_data[['data_map']], fips_state %in% '02'), aes_string(fill = tab_data[['col_value']]), size = 0.25) +
-            color_scale +
-            coord_sf(crs = ak_crs) +
-            map_theme +
-            theme(legend.position = 'none')
-        
-        phi <- ggplot() +
-            geom_sf(data = subset(tab_data[['data_map']], fips_state %in% '15'), aes_string(fill = tab_data[['col_value']]), size = 0.25) +
-            color_scale +
-            coord_sf(crs = hi_crs, xlim = hi_box[c('xmin','xmax')], ylim = hi_box[c('ymin','ymax')]) +
-            map_theme +
-            theme(legend.position = 'none')
-        
-        # FIXME This layout was done very lazily and needs to be cleaned up. Align panels better and make L48 map bigger and insets smaller.
-        #grid.arrange(p48, pak, phi, layout_matrix = rbind(c(1,1), c(2,3)), heights = c(3, 1))
-        grid.arrange(p48, pak, phi, layout_matrix = cbind(c(NA, 2, 3), c(1, 1, 1)), widths = c(1, 4))
+                tab_data[['data']] <- merge(global_country_map, tab_data[['data']], by = 'ISO_A3', all.x = TRUE)
+                
+                ggplot() +
+                    geom_sf(data = tab_data[['data']], aes_string(fill = tab_data[['col_value']]), size = 0.25) +
+                    color_scale +
+                    map_theme +
+                    theme(legend.position = 'top', legend.key.width = unit(1.2, 'cm'))
+
+            }
+        } else {
+            
+            tab_data[['data']] <- merge(county_map, tab_data[['data']], by = 'county', all.x = TRUE)
+
+            p48 <- ggplot() +
+                geom_sf(data = subset(tab_data[['data']], !fips_state %in% c('02', '15')), aes_string(fill = tab_data[['col_value']]), size = 0.25) +
+                color_scale +
+                map_theme +
+                theme(legend.position = 'top', legend.key.width = unit(1.2, 'cm'))
+            
+            pak <- ggplot() +
+                geom_sf(data = subset(tab_data[['data']], fips_state %in% '02'), aes_string(fill = tab_data[['col_value']]), size = 0.25) +
+                color_scale +
+                coord_sf(crs = ak_crs) +
+                map_theme +
+                theme(legend.position = 'none')
+            
+            phi <- ggplot() +
+                geom_sf(data = subset(tab_data[['data']], fips_state %in% '15'), aes_string(fill = tab_data[['col_value']]), size = 0.25) +
+                color_scale +
+                coord_sf(crs = hi_crs, xlim = hi_box[c('xmin','xmax')], ylim = hi_box[c('ymin','ymax')]) +
+                map_theme +
+                theme(legend.position = 'none')
+            
+            # FIXME This layout was done very lazily and needs to be cleaned up. Align panels better and make L48 map bigger and insets smaller.
+            #grid.arrange(p48, pak, phi, layout_matrix = rbind(c(1,1), c(2,3)), heights = c(3, 1))
+            grid.arrange(p48, pak, phi, layout_matrix = cbind(c(NA, 2, 3), c(1, 1, 1)), widths = c(1, 4))
+        }
     })
 }
 
